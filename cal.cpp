ea831f2093bcc2c690211c51a7723499ac54b80f
“SteveLuo7” <291581211@qq.com>
2023-03-15 22:49:25 +0800

    first commit

---
+++ cal.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Shibin Luo on 12/27/22.
+//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+
+    cin >> a >> b;
+    cout << a + b << endl;
+
+
+    return 0;
+}
---
+++ repeat.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Shibin Luo on 12/27/22.
+//
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    char ch;
+
+    while(cin.get(ch)) {
+        cout.put(ch);
+    }
+
+
+    return 0;
+}
